3f3c63f517bd411c9e96709a1ba4635545d3e7ee
Akankobateng Rueben <akankobatengr@gmail.com>
2022-08-24 17:21:29 +0000

    Initial Commit

---
+++ Bucketlist/BucketlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketlistApp.swift
+//  Bucketlist
+//
+//  Created by RUEBEN on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
